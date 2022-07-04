91b0219483f114dec53078fd2921b40ae0c56554
Boris Solomonia <borissolomoniaphone@gmail.com>
2022-07-04 19:46:06 +0400

    first commit

---
+++ src/main/java/EnclosingClass.java
@@ -0,0 +1,13 @@
+public class EnclosingClass {
+    //This class is an Enclosing class for virtual wallet
+    public Double transferMoneyAmount;
+    public static Double cryptoBalance;
+    private Double transferRequest;
+
+    public EnclosingClass(Double transferMoneyAmount, Double transferRequest) {
+        this.transferMoneyAmount = transferMoneyAmount;
+        this.transferRequest = transferRequest;
+    }
+
+
+}
