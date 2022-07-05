33a2ded3d7161dfa788fd7743b0aec7a63e0efe0
Boris Solomonia <borissolomoniaphone@gmail.com>
2022-07-05 09:47:24 +0400

    second commit

--- src/main/java/EnclosingClass.java
+++ src/main/java/EnclosingClass.java
@@ -1,13 +1,48 @@
 public class EnclosingClass {
     //This class is an Enclosing class for virtual wallet
-    public Double transferMoneyAmount;
+    public Double initBalance;
     public static Double cryptoBalance;
     private Double transferRequest;
 
-    public EnclosingClass(Double transferMoneyAmount, Double transferRequest) {
-        this.transferMoneyAmount = transferMoneyAmount;
+    public EnclosingClass(Double initBalance, Double transferRequest) {
+        this.initBalance = initBalance;
         this.transferRequest = transferRequest;
+        cryptoBalance = initBalance;
+
     }
 
+    public void withdrawMoney(Double amountRequested){
+        // withdraws money from the account
+        if(amountRequested > cryptoBalance){
+            cryptoBalance -=amountRequested;
+        }
+
+    }
+
+
+    protected class Atm {
+        public Double returnBalance(){
+            return cryptoBalance;
+        };
+
+        public Double withdraw(Double amount){
+            return cryptoBalance -= amount;
+        };
+    }
+
+    Atm atm = new Atm() {
+        public Double returnBalance(){
+            return cryptoBalance;
+        };
+    };
+
+    public static class Transferwise {
+        public Double returnBalance(){
+            return cryptoBalance;
+        };
+        public Double withdraw(Double amount){
+            return cryptoBalance -= amount;
+        };
+    };
 
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        EnclosingClass eclass = new EnclosingClass(500.0,0.0);
+        //EnclosingClass.Transferwise foreign = new EnclosingClass.Transferwise();
+        EnclosingClass.Atm atm = eclass.new Atm();
+        System.out.println(atm.withdraw(100.0));
+    }
+}
